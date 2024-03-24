be25de33c4c5459577e4a89d8cbccd6fed36ad58
Pythonist <134071387+Rounio77@users.noreply.github.com>
2024-03-25 00:52:28 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,57 +1,39 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox as mb
 
-class MyGUI:
-    def __init__(self):
-        self.root = tk.Tk()
-        
-        self.menu_bar = tk.Menu(self.root)
-        
-        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.file_menu.add_command(label="Close", command=self.on_closing)
-        self.file_menu.add_separator()
-        self.file_menu.add_command(label="Close Without Question", command=exit)
-        
-        self.menu_bar.add_cascade(menu=self.file_menu, label="File")
-        self.root.config(menu=self.menu_bar)
-        
-        self.label = tk.Label(self.root, text="hello world", font=("Arial", 10))
-        self.label.pack(padx=20, pady=20)
-        
-        self.text_box = tk.Text(self.root, height=3, font=("Arial", 16))
-        self.text_box.bind("<KeyPress>", self.shortcut)
-        self.text_box.pack(padx=10)
-        
-        self.check_state = tk.IntVar()
-        
-        self.check= tk.Checkbutton(self.root, text="show message", font=("Arial", 16), variable=self.check_state)
-        self.check.pack(padx=10, pady=10)
-        
-        self.button = tk.Button(self.root, text="show message", font=("Arial", 18), command=self.show_message)
-        self.button.pack(padx=10, pady=10)
-        
-        self.clear_button = tk.Button(self.root, text="clear", font=("Arial", 16), command=self.clear)
-        self.clear_button.pack(padx=10, pady=10)
+class Login(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.username = tk.Label(self, text="Username", font=("Ariel", 16))
+        self.password = tk.Label(self, text="Password", font=("Ariel", 16))
 
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.root.mainloop()
-        
-    def show_message(self):
-        if self.check_state.get() == 0:
-            print(self.text_box.get("1.0", tk.END))
+        self.username_entry = tk.Entry(self)
+        self.password_entry = tk.Entry(self, show="#")
+
+        self.username.grid(row=0, sticky=tk.E)
+        self.password.grid(row=1, sticky=tk.E)
+        self.username_entry.grid(row=0, column=1)
+        self.password_entry.grid(row=1, column=1)
+
+        self.button = tk.Button(self, text="Login", command=self.login)
+        self.button.grid(columnspan=2)
+
+        self.pack()
+
+    def login(self):
+        get_username = self.username_entry.get()
+        get_password = self.password_entry.get()
+        if get_username == "User_name" and get_password == "Pass_word":
+            mb.showinfo("Login", "login successful")
         else:
-            messagebox.showinfo(title="message", message=self.text_box.get("1.0", tk.END))
-            
-    def shortcut(self, event):
-        if event.state == 12 and event.keysym == "Return":
-            self.show_message()
-            
+            mb.showerror("Login", "login failed")
+
     def on_closing(self):
-        if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
-            self.root.destroy()
-            
-    def clear(self):
-        self.text_box.delete("1.0", tk.END)
-        
-            
-MyGUI()
+        mb.askyesno(title="Quit", message="Are you sure you want to quit?")
+        self.master.destroy()
+
+app = tk.Tk()
+login = Login(app)
+app.protocol("WM_DELETE_WINDOW", login.on_closing)
+app.mainloop()
+
